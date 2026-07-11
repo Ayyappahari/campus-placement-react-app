a150bfc751873124cdabff25d4516f568943ce82
Ayyappahari <prayyappahari@gmail.com>
2026-07-11 11:52:29 +0530

    add reg

--- src/App.jsx
+++ src/App.jsx
@@ -3,15 +3,15 @@ import reactLogo from './assets/react.svg'
 import viteLogo from './assets/vite.svg'
 import heroImg from './assets/hero.png'
 import './App.css'
+import AddRegistration from './components/AddRegistration'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <>
-      
+      <AddRegistration />
     </>
   )
-}
 
 export default App
