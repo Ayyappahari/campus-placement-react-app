335b4e9d6a2034820f9f43f877bc9947727e0c5b
Ayyappahari <prayyappahari@gmail.com>
2026-07-11 12:12:37 +0530

    search reg

---
+++ src/components/SearchRegistration.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const SearchRegistration = () => {
+  return (
+    <div>
+        <h1>Search Registration</h1>
+    </div>
+  )
+}
+
+export default SearchRegistration
